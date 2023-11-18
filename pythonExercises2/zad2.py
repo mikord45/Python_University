4c4b2e05e4e519130ca2c42511fa8a10b4527b01
mikord45 <51155742+mikord45@users.noreply.github.com>
2023-11-18 02:57:50 +0100

    zad4 - fixed

--- pythonExercises2/zad2.py
+++ pythonExercises2/zad2.py
@@ -5,6 +5,11 @@ def read_config(file_name):
         lines = file.readlines()
         return [line.strip() for line in lines]
 
+def read_message(file_name):
+    with open(file_name, 'r') as file:
+        text = file.read()
+        return text
+
 def send_email(sender, receiver, subject, message, password, smtp_server, smtp_port):
     body = f"From: {sender}\nTo: {receiver}\nSubject: {subject}\n\n{message}"
     try:
@@ -18,12 +23,12 @@ def send_email(sender, receiver, subject, message, password, smtp_server, smtp_p
         print(f"Wystąpił błąd podczas wysyłania e-maila: {e}")
 
 config_data = read_config('config.txt')
+email_message = read_message('email_message.txt')
 
 if len(config_data) == 4:
     sender_email, password, smtp_server, smtp_port = config_data
     receiver_email = '527e24de-f5e4-448b-8e65-7f915618225d@mailslurp.mx'
     email_subject = 'Topic Test'
-    email_message = 'Message Test.'
     send_email(sender_email, receiver_email, email_subject, email_message, password, smtp_server, int(smtp_port))
 else:
     print("Nie udało się odczytać wszystkich danych z pliku konfiguracyjnego.")
